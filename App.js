f67557924a02a9857b4b34c38df05bffef9358eb
Imran Yousuf <iyousuf@ebay.com>
2018-01-02 13:05:18 -0800

    done

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
+    let imran = 'ok';
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>{imran}</Text>
       </View>
     );
   }
